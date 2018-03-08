38145dd864594ebc63e114b137107738cc58e60c
Kachel Wittig <kjwittig@gmail.com>
2018-03-08 21:00:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    array.last = array.last + ", and"
-    array.join(", ") + array.last
+    array.last_el = array.last + ", and"
+    array.join(", ") + array.last_el
   else
     array.join
   end
